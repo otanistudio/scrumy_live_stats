7481a5e40b6e8735c15f024ce3264fc8d47892f0
otanistudio <otanistudio@gmail.com>
2011-02-07 19:08:54 -0800

    messing around with the story rows or something like that

--- contentscript.js
+++ contentscript.js
@@ -14,6 +14,15 @@ chrome.extension.onConnect.addListener(
     function(port) {
         port.onMessage.addListener(
             function(msg) {
+                var storyRows = $('tr.story-task-row').toArray();
+                var stories = [];
+                for(var i = 0; i < storyRows.length; i++) {
+                    var storyCols = $(storyRows[i]).children().toArray();
+                    for(var j = 0; j < storyCols[j].length; j++) {
+                        
+                    }                    
+                }
+                
                 var taskTimes = {
                     "done" : countEm($('td.done pre.title_inner')),
                     "todo" : countEm($('td.todo pre.title_inner')),
